4f3323a6bd361a21f205d12ec6c467c83187999f
doubled <gubrak.sukur9@gmail.com>
2021-03-06 01:28:44 +0700

    bellom kapok

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -6,50 +6,60 @@
             :autoplay-speed="5000"
             :dots="false"
         >
-            <div class="banner__slider">
-                <div class="banner__slider-overlay banner__slider-overlay--green">
-                </div>
-                <div class="banner__card">
-                    <div class="banner__card-header">
-                        <div class="banner__card-title">
-                            <p>masjid al falaah</p>
-                            <p>kampung dadap - glugur darat medan timur</p>
+            <template v-slot:default>
+                <div class="banner__slider">
+                    <div class="banner__slider-overlay banner__slider-overlay--green">
+                    </div>
+                    <div class="banner__card">
+                        <div class="banner__card-header">
+                            <div class="banner__card-title">
+                                <p>masjid al falaah</p>
+                                <p>kampung dadap - glugur darat medan timur</p>
+                            </div>
+                        </div>
+                        <div class="banner__card-body">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s, when an unknown printer..</p>
+                        </div>
+                        <div class="banner__card-footer">
+                            <button class="banner__button">profile masjid</button>
                         </div>
                     </div>
-                    <div class="banner__card-body">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer..</p>
-                    </div>
-                    <div class="banner__card-footer">
-                        <button class="banner__button">profile masjid</button>
-                    </div>
                 </div>
-            </div>
-            <div class="banner__slider">
-                <div class="banner__slider-overlay banner__slider-overlay--blue">
-                </div>
-                <div class="banner__card">
-                    <div class="banner__card-header">
-                        <div class="banner__card-title">
-                            <p>masjid al falaah</p>
-                            <p>kampung dadap - glugur darat medan timur</p>
+                <div class="banner__slider">
+                    <div class="banner__slider-overlay banner__slider-overlay--blue">
+                    </div>
+                    <div class="banner__card">
+                        <div class="banner__card-header">
+                            <div class="banner__card-title">
+                                <p>masjid al falaah</p>
+                                <p>kampung dadap - glugur darat medan timur</p>
+                            </div>
+                        </div>
+                        <div class="banner__card-body">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s, when an unknown printer..</p>
+                        </div>
+                        <div class="banner__card-footer">
+                            <button class="banner__button">profile masjid</button>
                         </div>
                     </div>
-                    <div class="banner__card-body">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy
-                            text ever since the 1500s, when an unknown printer..</p>
-                    </div>
-                    <div class="banner__card-footer">
-                        <button class="banner__button">profile masjid</button>
-                    </div>
                 </div>
-            </div>
+            </template>
 
-            
+            <template v-slot:prevButton>
+                <font-awesome-layers class="">
+                    <font-awesome-icon :icon="faArrowLeft"/>
+                </font-awesome-layers>
+            </template>
 
-           
+            <template v-slot:nextButton>
+                <font-awesome-layers class="">
+                    <font-awesome-icon :icon="faArrowRight" />
+                </font-awesome-layers>
+            </template>
             
         </agile>
     </div>
