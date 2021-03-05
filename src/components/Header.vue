3f0b5d9cf772d87cb59ca8fb2a07e16c7f2a5f1e
dede <gubrak.sukur9@gmail.com>
2021-03-06 00:35:34 +0700

    resolve arrow on slider

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,36 +22,3 @@
         </div>
     </div>
 </template>
-
-<style lang="scss">
-@import "../assets/style/components/font-face.scss";
-
-@layer components {
-    .header {
-        @include raleway-text;
-        @apply flex justify-between w-10/12 mx-auto py-9;
-        @apply font-bold capitalize text-gray-500;
-
-        &__list {
-            @apply flex items-center;
-        }
-
-        &__item {
-            @apply px-8;
-        }
-
-        &__link {
-            @apply cursor-pointer;
-        }
-
-        &__link:hover {
-            color: #1C5C1C;
-            @apply font-extrabold;
-        }
-
-        &__search {
-            @apply my-auto;
-        }
-    }
-}
-</style>
